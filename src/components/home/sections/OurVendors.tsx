fb433a7eb901508f786f750b046bf532acd0848e
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2025-11-05 10:00:57 +0500

    add slider

--- src/components/home/sections/OurVendors.tsx
+++ src/components/home/sections/OurVendors.tsx
@@ -1,9 +1,27 @@
+import { useRef } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
 import Huawei from "../../../assets/logos/huawei_logo.png"
+import IBM from "../../../assets/logos/ibm-logo-1.png"
+import Brocade from "../../../assets/logos/brocade-logo-1.png"
+import Juniper from "../../../assets/logos/juniper-logo.png"
+import Xfusion from "../../../assets/logos/xfusion-logo.png"
 import LogoCard from "../../LogoCard"
 
 const OurVendors = () => {
+  const prevButtonRef = useRef<HTMLButtonElement>(null)
+  const nextButtonRef = useRef<HTMLButtonElement>(null)
+
+  const vendors = [
+    { image: Huawei, href: "#" },
+    { image: IBM, href: "#" },
+    { image: Brocade, href: "#" },
+    { image: Juniper, href: "#" },
+    { image: Xfusion, href: "#" },
+  ]
+
   return (
-    <section className="sticky top-[90px] min-h-dvh z-5 bg-brand-light-black flex flex-col items-center justify-center">
+    <section className="sticky top-[90px] min-h-dvh z-5 bg-brand-black flex flex-col items-center justify-center">
       <div className={`relative max-w-[766px] w-full mx-auto text-white`}>
         <span className={`block absolute transition-all duration-500 left-0 top-0 stroke-brand-white`}>
           <svg xmlns="http://www.w3.org/2000/svg" width={25} height={24} viewBox="0 0 25 24" fill="none">
@@ -35,9 +53,64 @@ const OurVendors = () => {
         </h2>
       </div>
 
-      <div className="max-w-7xl mx-auto">
-        {/* swiper item */}
-        <LogoCard Image={Huawei} href="#" />
+      <div className="max-w-7xl mx-auto mt-[89px] w-full px-10 relative">
+        <button
+          ref={prevButtonRef}
+          className="absolute left-0 top-1/2 -translate-y-1/2 z-10 w-12 h-full flex items-center justify-center bg-brand-light-black rounded-lg hover:bg-brand-normal transition-colors duration-300 group cursor-pointer"
+          aria-label="Previous slide"
+        >
+          <svg className='rotate-180' xmlns="http://www.w3.org/2000/svg" width={19} height={14} viewBox="0 0 19 14" fill="none">
+            <path d="M11.388 1.57262L16.6679 6.85246L11.388 12.1323M15.8556 6.85246L1.11125 6.85261" stroke="#FBFFFE" strokeWidth="2.22222" strokeLinecap="square" />
+          </svg>
+        </button>
+        
+        <button
+          ref={nextButtonRef}
+          className="absolute right-0 top-1/2 -translate-y-1/2 z-10 w-12 h-full flex items-center justify-center bg-brand-light-black rounded-lg hover:bg-brand-normal transition-colors duration-300 group cursor-pointer"
+          aria-label="Next slide"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width={19} height={14} viewBox="0 0 19 14" fill="none">
+            <path d="M11.388 1.57262L16.6679 6.85246L11.388 12.1323M15.8556 6.85246L1.11125 6.85261" stroke="#FBFFFE" strokeWidth="2.22222" strokeLinecap="square" />
+          </svg>
+        </button>
+
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={0}
+          slidesPerView={1}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+          navigation={{
+            prevEl: prevButtonRef.current,
+            nextEl: nextButtonRef.current,
+          }}
+          onSwiper={(swiper) => {
+            // Initialize navigation after Swiper is ready
+            if (prevButtonRef.current && nextButtonRef.current) {
+              // @ts-ignore - Swiper types issue with navigation
+              swiper.params.navigation.prevEl = prevButtonRef.current
+              // @ts-ignore - Swiper types issue with navigation
+              swiper.params.navigation.nextEl = nextButtonRef.current
+              swiper.navigation.init()
+              swiper.navigation.update()
+            }
+          }}
+          className="vendor-swiper"
+        >
+          {vendors.map((vendor, index) => (
+            <SwiperSlide key={index}>
+              <div className="flex justify-center">
+                <LogoCard Image={vendor.image} href={vendor.href} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   )
